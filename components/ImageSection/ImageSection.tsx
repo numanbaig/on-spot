06a8646cfb711920914aad05e99a7ae94abd800d
Noman <numansafi97@gmail.com>
2022-06-07 13:14:19 +0500

    fix: image section

---
+++ components/ImageSection/ImageSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from "next/link"
+const ImageSection = () => {
+    return (
+        <div className="bg-white flex justify-center">
+
+        </div>
+    )
+}
+export default ImageSection;
