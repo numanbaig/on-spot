3dc98bd09e1ebff61a65008173c059f893350631
numan baig <numansafi97@gmail.com>
2022-06-15 18:35:55 +0500

    fixe some speces

--- components/Dedicamos/Dedicamos.js
+++ components/Dedicamos/Dedicamos.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Button from '../Common/Button'
+import Button from "../Common/Button";
 
 const Dedicamos = () => {
   return (
-    <div className="bg-[url('/images/Section5Background.png')] h-[700px] bg-no-repeat w-[100%] bg-cover relative pt-[70px] top-[-176px]">
+    <div className="bg-[url('/images/Section5Background.png')] h-[700px] bg-no-repeat w-[100%] bg-cover relative pt-[70px] top-[-193px]">
       <div className=" w-[700px] relative">
         <div className="max-w-[70%]">
-          <Button heading={'¿A QUÉ NOS'} subheading={'DEDICAMOS?'}/>
+          <Button heading={"¿A QUÉ NOS"} subheading={"DEDICAMOS?"} />
         </div>
-        <div className="flex flex-row-reverse">
+        <div className="flex flex-row-reverse pt-5">
           <p className="w-[500px] text-end font-normal text-[14px]">
             Nuestro objetivo es garantizar una manera legal, segura y oportuna
             de que nuestros usuarios dispongan del dinero producido en las
